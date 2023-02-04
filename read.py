9d3fa306ef637b05917c6938afe0c1a4725d26f6
cascade7 <yiminlee777@gmail.com>
2023-02-04 21:34:54 +0800

    version2

--- read.py
+++ read.py
@@ -1,10 +1,30 @@
 data = []
-sum_len = 0
+count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('總留言筆數:', len(data))
+
+sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('總留言長度:', sum_len)
-print('總留言筆數:', len(data))
 print('平均留言長度:', sum_len/len(data))
+
+new = []
+for d in data :
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言小於100個字')
+print('第一筆:', new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good),' 筆留言提到good')
+print('第一筆:', good[0])
+
