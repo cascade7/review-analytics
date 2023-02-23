6c61e0a6358cf3e5be5c4c5d8d8988381f203579
cascade7 <yiminlee777@gmail.com>
2023-02-23 15:55:25 +0800

    add progressbar

--- read.py
+++ read.py
@@ -1,11 +1,15 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		bar.update(count)
+bar.finish()
 print('總留言筆數:', len(data))
 
 #留言統計
@@ -21,20 +25,21 @@ for d in data :
 		new.append(d)
 print('一共有', len(new), '筆留言小於100個字')
 print('第一筆:', new[0])
-
+"""
 good = []
 for d in data:
 	if 'good' in d:
 		good.append(d)
 print('一共有', len(good),' 筆留言提到good')
 print('第一筆:', good[0])
-
+"""
 good = [d for d in data if 'good' in d] #快寫法
 print('一共有', len(good),' 筆留言提到good')
 print('第一筆:', good[0])
 bad = ['bad' in d for d in data]
 
 # 用字計數
+start_time = time.time()
 wc = {}
 for d in data :
 	words = d.split()
@@ -47,6 +52,8 @@ for d in data :
 for word in wc:
 	if wc[word] > 1000000:
 		print(word, wc[word])
+end_time = time.time()
+print('花了', end_time-start_time, 'seconds')
 print(len(wc))
 
 while True:
@@ -58,50 +65,3 @@ while True:
 	else:
 		print(word, ':', wc[word])
 print('感謝你使用本查詢功能!!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-sum_len = 0
-for d in data:
-	sum_len = sum_len + len(d)
-print('總留言長度:', sum_len)
-print('平均留言長度:', sum_len/len(data))
-
-new = []
-for d in data :
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言小於100個字')
-print('第一筆:', new[0])
-
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
-print('一共有', len(good),' 筆留言提到good')
-print('第一筆:', good[0])
-
-good = [d for d in data if 'good' in d] #快寫法
-print('一共有', len(good),' 筆留言提到good')
-print('第一筆:', good[0])
-bad = ['bad' in d for d in data]
-"""
